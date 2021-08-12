db7dc61e882a4e1e5d4c0a169856ec87de1cb714
fost <fosteev3@gmail.com>
2021-08-12 11:17:07 +0300

    Backend fix cors

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,12 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
